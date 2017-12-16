2563d5aa1e705e9be556c34072eed902fd2318f5
renfufei <renfufei@qq.com>
2017-12-16 15:34:52 +0800

    调整基本参数与属性

--- src/main/java/com/cncounter/cncounter/service/impl/other/FavoriteServiceImpl.java
+++ src/main/java/com/cncounter/cncounter/service/impl/other/FavoriteServiceImpl.java
@@ -5,6 +5,7 @@ import com.cncounter.cncounter.model.other.Favorite;
 import com.cncounter.cncounter.service.api.other.FavoriteService;
 import com.cncounter.cncounter.service.impl.base.ServiceImplBase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 public class FavoriteServiceImpl extends ServiceImplBase implements FavoriteService {
 
 	@Autowired
+    @Qualifier("oldFavoriteMapper")
 	private FavoriteMapper favoriteMapper;
 	
 	
