9607a97dcae025799ec57d0754f73c591e633861
renfufei <renfufei@qq.com>
2018-08-16 16:34:16 +0800

    增加字段访问修饰标志位

--- src/main/java/com/cncounter/util/bytecode/AccessFlagsEnum.java
+++ src/main/java/com/cncounter/util/bytecode/AccessFlagsEnum.java
@@ -4,13 +4,22 @@ package com.cncounter.util.bytecode;
  * 访问标识
  */
 public enum AccessFlagsEnum {
+    //
     ACC_PUBLIC(0x0001, "public"),
+    ACC_PRIVATE(0x0002, "private"),
+    ACC_PROTECTED(0x0004, "protected"),
+    ACC_STATIC(0x0008, "static"),
+    //
     ACC_FINAL(0x0010, "final"),
     // super. 指令调用需要特殊处理(invokespecial)
     ACC_SUPER(0x0020, "[super.]"),
+    ACC_VOLATILE(0x0040, "volatile"),
+    ACC_TRANSIENT(0x0080, "transient"),
+    //
     ACC_INTERFACE(0x0200, "interface"),
     ACC_ABSTRACT(0x0400, "abstract"),
-    ACC_SYNTHETIC(0x1000, "synchronized"),
+    // 如 <init> 等编译器生成的方法,或泛型类
+    ACC_SYNTHETIC(0x1000, "<synthetic>"),
     ACC_ANNOTATION(0x2000, "@interface"),
     ACC_ENUM(0x4000, "enum");
 
@@ -28,14 +37,19 @@ public enum AccessFlagsEnum {
     public static String parseAccessFlags(int accessFlagsNumber){
         String result = "";
         //
-        boolean isClass = true;
-
-        //
+        // 可见范围
         if(AccessFlagsEnum.ACC_PUBLIC.in(accessFlagsNumber)){
             result += " " + AccessFlagsEnum.ACC_PUBLIC.type;
         }
-        if(AccessFlagsEnum.ACC_SUPER.in(accessFlagsNumber)){
-            //result += " " + AccessFlagsEnum.ACC_SUPER.type;
+        if(AccessFlagsEnum.ACC_PRIVATE.in(accessFlagsNumber)){
+            result += " " + AccessFlagsEnum.ACC_PRIVATE.type;
+        }
+        if(AccessFlagsEnum.ACC_PROTECTED.in(accessFlagsNumber)){
+            result += " " + AccessFlagsEnum.ACC_PROTECTED.type;
+        }
+        //
+        if(AccessFlagsEnum.ACC_STATIC.in(accessFlagsNumber)){
+            result += " " + AccessFlagsEnum.ACC_STATIC.type;
         }
         if(AccessFlagsEnum.ACC_FINAL.in(accessFlagsNumber)){
             result += " " + AccessFlagsEnum.ACC_FINAL.type;
@@ -43,23 +57,29 @@ public enum AccessFlagsEnum {
         if(AccessFlagsEnum.ACC_ABSTRACT.in(accessFlagsNumber)){
             result += " " + AccessFlagsEnum.ACC_ABSTRACT.type;
         }
-        if(AccessFlagsEnum.ACC_SYNTHETIC.in(accessFlagsNumber)) {
-            result += " " + AccessFlagsEnum.ACC_SYNTHETIC.type;
+        if(AccessFlagsEnum.ACC_VOLATILE.in(accessFlagsNumber)) {
+            result += " " + AccessFlagsEnum.ACC_VOLATILE.type;
         }
+        if(AccessFlagsEnum.ACC_TRANSIENT.in(accessFlagsNumber)) {
+            result += " " + AccessFlagsEnum.ACC_TRANSIENT.type;
+        }
+        //
         if(AccessFlagsEnum.ACC_ANNOTATION.in(accessFlagsNumber)) {
             result += " " + AccessFlagsEnum.ACC_ANNOTATION.type;
-            isClass = false;
         }
         if(AccessFlagsEnum.ACC_ENUM.in(accessFlagsNumber)) {
             result += " " + AccessFlagsEnum.ACC_ENUM.type;
-            isClass = false;
         }
         if(AccessFlagsEnum.ACC_INTERFACE.in(accessFlagsNumber)) {
             result += " " + AccessFlagsEnum.ACC_INTERFACE.type;
-            isClass = false;
         }
-        if(isClass) {
-            result += " " + "class";
+        // 特殊标记
+        //
+        if(AccessFlagsEnum.ACC_SUPER.in(accessFlagsNumber)){
+            //result += " " + AccessFlagsEnum.ACC_SUPER.type;
+        }
+        if(AccessFlagsEnum.ACC_SYNTHETIC.in(accessFlagsNumber)) {
+            //result += " " + AccessFlagsEnum.ACC_SYNTHETIC.type;
         }
 
         //
@@ -83,6 +103,7 @@ public enum AccessFlagsEnum {
 
 /*
 
+# 类访问修饰
 Table 4.1-A. Class access and property modifiers
 
 | Flag Name        | Value  | Interpretation                           |
@@ -95,5 +116,23 @@ Table 4.1-A. Class access and property modifiers
 |  ACC_SYNTHETIC   | 0x1000 | Declared synthetic; not present in the source code. |
 |  ACC_ANNOTATION  | 0x2000 | Declared as an annotation type.          |
 |  ACC_ENUM        | 0x4000 | Declared as an  enum  type.              |
+
+
+
+# 字段访问修饰
+Table 4.5-A. Field access and property flags
+
+| Flag Name       | Value  | Interpretation                           |
+| --------------- | ------ | ---------------------------------------- |
+|  ACC_PUBLIC     | 0x0001 | Declared  public ; may be accessed from outside its package. |
+|  ACC_PRIVATE    | 0x0002 | Declared  private ; usable only within the defining class. |
+|  ACC_PROTECTED  | 0x0004 | Declared  protected ; may be accessed within subclasses. |
+|  ACC_STATIC     | 0x0008 | Declared  static .                       |
+|  ACC_FINAL      | 0x0010 | Declared  final ; never directly assigned to after object construction (JLS §17.5). |
+|  ACC_VOLATILE   | 0x0040 | Declared  volatile ; cannot be cached.   |
+|  ACC_TRANSIENT  | 0x0080 | Declared  transient ; not written or read by a persistent object manager. |
+|  ACC_SYNTHETIC  | 0x1000 | Declared synthetic; not present in the source code. |
+|  ACC_ENUM       | 0x4000 | Declared as an element of an  enum .     |
+
 
 */
