8aa3edaaacc03575588f1e7867863878c6ef36b9
renfufei <renfufei@qq.com>
2018-08-17 11:20:32 +0800

    方法访问标识

--- src/main/java/com/cncounter/util/bytecode/enums/AccessFlagsEnum.java
+++ src/main/java/com/cncounter/util/bytecode/enums/AccessFlagsEnum.java
@@ -5,88 +5,106 @@ package com.cncounter.util.bytecode.enums;
  */
 public enum AccessFlagsEnum {
     //
-    ACC_PUBLIC(0x0001, "public"),
-    ACC_PRIVATE(0x0002, "private"),
-    ACC_PROTECTED(0x0004, "protected"),
-    ACC_STATIC(0x0008, "static"),
+    ACC_PUBLIC(0x0001, "public", "class|field|method"),
+    ACC_PRIVATE(0x0002, "private", "field|method"),
+    ACC_PROTECTED(0x0004, "protected", "field|method"),
+    ACC_STATIC(0x0008, "static", "field|method"),
     //
-    ACC_FINAL(0x0010, "final"),
+    ACC_FINAL(0x0010, "final", "class|field|method"),
     // super. 指令调用需要特殊处理(invokespecial)
-    ACC_SUPER(0x0020, "[super.]"),
-    ACC_VOLATILE(0x0040, "volatile"),
-    ACC_TRANSIENT(0x0080, "transient"),
+    ACC_SUPER(0x0020, "[super.]", "class"),
+    ACC_SYNCHRONIZED(0x0020, "synchronized", "method"),//; method
+    ACC_VOLATILE(0x0040, "volatile", "field"), // ; field
+    ACC_BRIDGE(0x0040, "bridge", "method"), // generate by compiler; method
+    ACC_TRANSIENT(0x0080, "transient", "field"),
+    ACC_VARARGS(0x0080, "...", "method"),// ; method; 可变参数
     //
-    ACC_INTERFACE(0x0200, "interface"),
-    ACC_ABSTRACT(0x0400, "abstract"),
+    ACC_NATIVE(0x0100, "native", "method"),
+    ACC_INTERFACE(0x0200, "interface", "class"),
+    ACC_ABSTRACT(0x0400, "abstract", "class|method"),
+    ACC_STRICT(0x0800, "strict", "method"),
     // 由编译器自动生成-标记
-    ACC_SYNTHETIC(0x1000, "<synthetic>"),
-    ACC_ANNOTATION(0x2000, "@interface"),
-    ACC_ENUM(0x4000, "enum");
+    ACC_SYNTHETIC(0x1000, "<synthetic>", "class|field|method"),
+    ACC_ANNOTATION(0x2000, "@interface", "class"), //; Class
+    ACC_ENUM(0x4000, "enum", "class|field");
 
+    // 由谁使用
+    public static final String USED_CLASS = "class";
+    public static final String USED_FIELD = "field";
+    public static final String USED_METHOD = "method";
+    //
     public int value;
     public String type;
+    public String usedBy;
 
-    AccessFlagsEnum(int value, String type) {
+    AccessFlagsEnum(int value, String type, String usedBy) {
         this.value = value;
         this.type = type;
+        this.usedBy = usedBy;
     }
 
-
-
     // 解析标志位
-    public static String parseAccessFlags(int accessFlagsNumber){
+    public static String parseAccessFlags(int accessFlagsNumber) {
         String result = "";
         //
         // 可见范围
-        if(AccessFlagsEnum.ACC_PUBLIC.in(accessFlagsNumber)){
+        if (AccessFlagsEnum.ACC_PUBLIC.in(accessFlagsNumber)) {
             result += " " + AccessFlagsEnum.ACC_PUBLIC.type;
         }
-        if(AccessFlagsEnum.ACC_PRIVATE.in(accessFlagsNumber)){
+        if (AccessFlagsEnum.ACC_PRIVATE.in(accessFlagsNumber)) {
             result += " " + AccessFlagsEnum.ACC_PRIVATE.type;
         }
-        if(AccessFlagsEnum.ACC_PROTECTED.in(accessFlagsNumber)){
+        if (AccessFlagsEnum.ACC_PROTECTED.in(accessFlagsNumber)) {
             result += " " + AccessFlagsEnum.ACC_PROTECTED.type;
         }
         //
-        if(AccessFlagsEnum.ACC_STATIC.in(accessFlagsNumber)){
+        if (AccessFlagsEnum.ACC_STATIC.in(accessFlagsNumber)) {
             result += " " + AccessFlagsEnum.ACC_STATIC.type;
         }
-        if(AccessFlagsEnum.ACC_FINAL.in(accessFlagsNumber)){
+        if (AccessFlagsEnum.ACC_FINAL.in(accessFlagsNumber)) {
             result += " " + AccessFlagsEnum.ACC_FINAL.type;
         }
-        if(AccessFlagsEnum.ACC_ABSTRACT.in(accessFlagsNumber)){
+        if (AccessFlagsEnum.ACC_ABSTRACT.in(accessFlagsNumber)) {
             result += " " + AccessFlagsEnum.ACC_ABSTRACT.type;
         }
-        if(AccessFlagsEnum.ACC_VOLATILE.in(accessFlagsNumber)) {
+        if (AccessFlagsEnum.ACC_VOLATILE.in(accessFlagsNumber)) {
             result += " " + AccessFlagsEnum.ACC_VOLATILE.type;
         }
-        if(AccessFlagsEnum.ACC_TRANSIENT.in(accessFlagsNumber)) {
+        if (AccessFlagsEnum.ACC_TRANSIENT.in(accessFlagsNumber)) {
             result += " " + AccessFlagsEnum.ACC_TRANSIENT.type;
         }
         //
-        if(AccessFlagsEnum.ACC_ANNOTATION.in(accessFlagsNumber)) {
+        if (AccessFlagsEnum.ACC_NATIVE.in(accessFlagsNumber)) {
+            result += " " + AccessFlagsEnum.ACC_NATIVE.type;
+        }
+        if (AccessFlagsEnum.ACC_ANNOTATION.in(accessFlagsNumber)) {
             result += " " + AccessFlagsEnum.ACC_ANNOTATION.type;
         }
-        if(AccessFlagsEnum.ACC_ENUM.in(accessFlagsNumber)) {
+        if (AccessFlagsEnum.ACC_ENUM.in(accessFlagsNumber)) {
             result += " " + AccessFlagsEnum.ACC_ENUM.type;
         }
-        if(AccessFlagsEnum.ACC_INTERFACE.in(accessFlagsNumber)) {
+        if (AccessFlagsEnum.ACC_INTERFACE.in(accessFlagsNumber)) {
             result += " " + AccessFlagsEnum.ACC_INTERFACE.type;
+        }
+        if (AccessFlagsEnum.ACC_STRICT.in(accessFlagsNumber)) {
+            result += " " + AccessFlagsEnum.ACC_STRICT.type;
         }
         // 特殊标记
         //
-        if(AccessFlagsEnum.ACC_SUPER.in(accessFlagsNumber)){
+        if (AccessFlagsEnum.ACC_SUPER.in(accessFlagsNumber)) {
             //result += " " + AccessFlagsEnum.ACC_SUPER.type;
         }
-        if(AccessFlagsEnum.ACC_SYNTHETIC.in(accessFlagsNumber)) {
+        if (AccessFlagsEnum.ACC_SYNTHETIC.in(accessFlagsNumber)) {
             //result += " " + AccessFlagsEnum.ACC_SYNTHETIC.type;
         }
 
         //
         return result;
     }
+
     /**
      * 拥有标志位
+     *
      * @param flags
      * @return
      */
@@ -98,7 +116,6 @@ public enum AccessFlagsEnum {
         }
     }
 }
-
 
 
 /*
@@ -134,5 +151,23 @@ Table 4.5-A. Field access and property flags
 |  ACC_SYNTHETIC  | 0x1000 | Declared synthetic; not present in the source code. |
 |  ACC_ENUM       | 0x4000 | Declared as an element of an  enum .     |
 
+
+# 方法访问修饰
+Table 4.6-A. Method access and property flags
+
+| Flag Name          | Value  | Interpretation                           |
+| ------------------ | ------ | ---------------------------------------- |
+|  ACC_PUBLIC        | 0x0001 | Declared  public ; may be accessed from outside its package. |
+|  ACC_PRIVATE       | 0x0002 | Declared  private ; accessible only within the defining class. |
+|  ACC_PROTECTED     | 0x0004 | Declared  protected ; may be accessed within subclasses. |
+|  ACC_STATIC        | 0x0008 | Declared  static .                       |
+|  ACC_FINAL         | 0x0010 | Declared  final ; must not be overridden ([§5.4.5](#jvms-5.4.5)). |
+|  ACC_SYNCHRONIZED  | 0x0020 | Declared  synchronized ; invocation is wrapped by a monitor use. |
+|  ACC_BRIDGE        | 0x0040 | A bridge method, generated by the compiler. |
+|  ACC_VARARGS       | 0x0080 | Declared with variable number of arguments. |
+|  ACC_NATIVE        | 0x0100 | Declared  native ; implemented in a language other than Java. |
+|  ACC_ABSTRACT      | 0x0400 | Declared  abstract ; no implementation is provided. |
+|  ACC_STRICT        | 0x0800 | Declared  strictfp ; floating-point mode is FP-strict. |
+|  ACC_SYNTHETIC     | 0x1000 | Declared synthetic; not present in the source code. |
 
 */
