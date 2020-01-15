78515dafad9a610021c6fa606c376f83bd7c805b
renfufei <renfufei@qq.com>
2020-01-15 13:59:49 +0800

    DeadLockSample

---
+++ src/test/java/demo/jvm0207/DeadLockSample.java
@@ -0,0 +1,48 @@
+package demo.jvm0207;
+
+import java.util.concurrent.TimeUnit;
+
+public class DeadLockSample {
+    private static Object lock1 = new Object();
+    private static Object lock2 = new Object();
+
+    private static class ThreadTask1 implements Runnable {
+        public void run() {
+            synchronized (lock1) {
+                System.out.println("lock1 by thread:" + Thread.currentThread().getId());
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock2) {
+                    System.out.println("lock2 by thread:" + Thread.currentThread().getId());
+                }
+            }
+        }
+    }
+
+    private static class ThreadTask2 implements Runnable {
+        public void run() {
+            synchronized (lock2) {
+                System.out.println("lock2 by thread:" + Thread.currentThread().getId());
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock1) {
+                    System.out.println("lock1 by thread:" + Thread.currentThread().getId());
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadTask1 task1 = new ThreadTask1();
+        ThreadTask2 task2 = new ThreadTask2();
+        //
+        new Thread(task1).start();
+        new Thread(task2).start();
+    }
+}
