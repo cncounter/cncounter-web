b3d6e85d048a429d8f403390f4a340b28ce5be15
renfufei <renfufei@qq.com>
2021-03-11 11:29:57 +0800

    SQL

--- tools/docker/mysql/init/01-databases.sql
+++ tools/docker/mysql/init/01-databases.sql
@@ -1,2 +1,10 @@
-CREATE DATABASE IF NOT EXISTS `cncounter`;
+CREATE DATABASE IF NOT EXISTS `cncounter` DEFAULT CHARACTER SET utf8mb4;
+USE `cncounter`;
+DROP USER IF EXISTS 'cncounter';
+CREATE USER 'cncounter'@'%' IDENTIFIED BY 'cncounter';
+-- grant all privileges on cncounter.* to 'cncounter'@'%' identified by 'cncounter';
+grant all privileges on cncounter.* to cncounter@localhost identified by 'cncounter';
+grant all privileges on cncounter.* to 'cncounter'@'cnc-web1' identified by 'cncounter';
+grant all privileges on cncounter.* to 'cncounter'@'cnc-web2' identified by 'cncounter';
+flush privileges;
 
